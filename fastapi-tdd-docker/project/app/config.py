74b8608a5de9d71e2e899ba2f085f0e4c7583bc8
Sidorov Nikita <sidorov@yandex.ru>
2021-02-17 00:11:14 +0300

    added postgres and tortoise orm

--- fastapi-tdd-docker/project/app/config.py
+++ fastapi-tdd-docker/project/app/config.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 from functools import lru_cache
 
 log = logging.getLogger('uvicorn')
@@ -9,6 +9,8 @@ log = logging.getLogger('uvicorn')
 class Settings(BaseSettings):
     environment: str = os.getenv('ENVIRONMENT', 'dev')
     testing: bool = os.getenv('TESTING', 0)
+    database_url: AnyUrl = os.environ.get('DATABASE_URL')
+
 
 @lru_cache()
 def get_settings() -> BaseSettings:
