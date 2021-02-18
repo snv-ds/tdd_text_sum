2fbf2493cd385dc99dd06b0d66413e9dcfc7e546
Sidorov Nikita <sidorov@yandex.ru>
2021-02-18 23:47:21 +0300

    added all methods

--- fastapi-tdd-docker/project/app/api/crud.py
+++ fastapi-tdd-docker/project/app/api/crud.py
@@ -1,3 +1,5 @@
+from typing import Union, List
+
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
 
@@ -9,3 +11,16 @@ async def post(payload: SummaryPayloadSchema) -> int:
     )
     await summary.save()
     return summary.id
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary[0]
+    else:
+        return None
+
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
--- fastapi-tdd-docker/project/app/api/summaries.py
+++ fastapi-tdd-docker/project/app/api/summaries.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException
+from typing import List
 
 from app.api import crud
+from app.models.tortoise import SummarySchema
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
 
 
@@ -16,3 +18,16 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
         'url': payload.url
     }
     return response_object
+
+
+@router.get('/{id}/', response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail='Summary not found')
+    return summary
+
+
+@router.get('/', response_model=List[SummarySchema])
+async def read_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
